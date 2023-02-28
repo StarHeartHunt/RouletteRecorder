8e94d3274030e6a9186b7e94b1085c339a42d847
StarHeartHunt <starheart233@gmail.com>
2023-02-28 12:56:13 +0800

    chore: update opcode of CN 6.25

--- RouletteRecorder/Constant/OpcodeStorage.cs
+++ RouletteRecorder/Constant/OpcodeStorage.cs
@@ -12,9 +12,9 @@ namespace RouletteRecorder.Constant
     {
         public static Dictionary<ushort, Opcode> China = new Dictionary<ushort, Opcode>
         {
-            { 0x0397, Opcode.ActorControlSelf },
-            { 0x00CF, Opcode.ContentFinderNotifyPop },
-            { 0x008B, Opcode.InitZone },
+            { 0x00F1, Opcode.ActorControlSelf },
+            { 0x02DD, Opcode.ContentFinderNotifyPop },
+            { 0x00CE, Opcode.InitZone },
         };
     }
 }
