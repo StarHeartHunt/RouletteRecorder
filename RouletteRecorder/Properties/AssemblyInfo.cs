81190562194d11cfe4ac17eec5dc7a4a4fb51b5e
StarHeartHunt <starheart233@gmail.com>
2023-04-12 11:28:06 +0800

    chore: update copyright year

--- RouletteRecorder/Properties/AssemblyInfo.cs
+++ RouletteRecorder/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("StarHeartHunt")]
 [assembly: AssemblyProduct("RouletteRecorder")]
-[assembly: AssemblyCopyright("Copyright © StarHeartHunt 2022")]
+[assembly: AssemblyCopyright("Copyright © StarHeartHunt 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
