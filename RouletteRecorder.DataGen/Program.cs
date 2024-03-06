5e0357909a16499767d0744fcd16a2f9139cfe74
StarHeartHunt <starheart233@gmail.com>
2024-03-06 11:33:24 +0800

    feat: init DataGenerator

---
+++ RouletteRecorder.DataGen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RouletteRecorder.DataGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
