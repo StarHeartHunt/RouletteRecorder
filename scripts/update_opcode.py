082242d0a8e504798dcc2ab1cc3831b573c76185
StarHeartHunt <starheart233@gmail.com>
2024-05-12 11:17:54 +0800

    ci: fix accidental submodule creates

--- scripts/update_opcode.py
+++ scripts/update_opcode.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 FILE_PATH = Path(__file__).parent
 
-OPCODE_PATH = FILE_PATH / "opcode" / "output"
+OPCODES_PATH = FILE_PATH / "opcodes" / "output"
 
 DEST_PATH = FILE_PATH.parent / "RouletteRecorder" / "Constant"
 GLOBAL_DEST_PATH = DEST_PATH / "OpcodeGlobal.cs"
@@ -11,7 +11,7 @@ CN_DEST_PATH = DEST_PATH / "OpcodeChina.cs"
 
 
 def update_opcode(pattern, src_name, dest):
-    opcodes = list(OPCODE_PATH.glob(pattern))
+    opcodes = list(OPCODES_PATH.glob(pattern))
     opcodes.sort(reverse=True)
 
     latest_opcode = opcodes[0]
