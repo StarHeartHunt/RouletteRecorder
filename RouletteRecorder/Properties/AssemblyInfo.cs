06336321d5f4cdb7c2ecade30ce2042be5c22c56
StarHeartHunt <starheart233@gmail.com>
2024-05-18 11:36:29 +0800

    build: remove costura and use ILRepack instead

--- RouletteRecorder/Properties/AssemblyInfo.cs
+++ RouletteRecorder/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("RouletteRecorder")]
 [assembly: AssemblyDescription("RouletteRecorder")]
 [assembly: AssemblyCompany("StarHeartHunt")]
-[assembly: AssemblyVersion("24.5.17.1123")]
+[assembly: AssemblyVersion("24.5.18.331")]
 [assembly: AssemblyCopyright("Copyright © StarHeartHunt 2024")]
--- RouletteRecorder/Utils/Database.cs
+++ RouletteRecorder/Utils/Database.cs
@@ -1,5 +1,6 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
 using RouletteRecorder.DAO;
 using System;
 using System.Collections.Generic;
@@ -57,6 +58,14 @@ namespace RouletteRecorder.Utils
             fs.Dispose();
 
             return true;
+        }
+    }
+
+    public class ItemNameConverter : DefaultTypeConverter
+    {
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return value.ToString();
         }
     }
 }
